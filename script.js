6e42f39c31a9c3315df1f3ef0701658c00471cbe
Shubhamjindal0801 <52204183+Shubhamjindal0801@users.noreply.github.com>
2023-04-24 22:15:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-//your code here
+
+    const priceCells = document.querySelectorAll('.prices');
+const totalPriceCell = document.querySelector('#grandTotal');
+
+function calculateTotalPrice() {
+  let totalPrice = 0;
+
+  priceCells.forEach((cell) => {
+    totalPrice += parseFloat(cell.textContent);
+  });
+
+  totalPriceCell.textContent = totalPrice.toFixed(2);
+}
+
+calculateTotalPrice();
+
+priceCells.forEach((cell) => {
+  cell.addEventListener('input', calculateTotalPrice);
+});
